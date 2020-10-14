c282f4af9daaeb8262122d6a3a49c490ea4284ee
Cadain <cmiller@aventmark.com>
2020-10-14 11:27:56 -0500

    Should be working now

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-//Install express server
 const express = require('express');
-const path = require('path');
- 
+
 const app = express();
- 
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/shop'));
- 
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/shop/index.html'));
+
+app.use(express.static(`${__dirname}/dist/shop`));
+
+app.get('*', (req, res) => {
+    res.sendFile(`./dist/shop/index.html`, { root: __dirname }); // load the single view file (angular will handle the page changes on the front-end)
 });
- 
-// Start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:4000/',
+  //apiUrl: 'http://localhost:4000/',
 
-  //apiUrl: 'https://instashop-backend.herokuapp.com/',
+  apiUrl: 'https://instashop-backend.herokuapp.com/',
   production: false,
 };
 
