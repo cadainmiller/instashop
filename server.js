e5c3187334216a59cf60e4b480410a5a72fcf777
Cadain <cmiller@aventmark.com>
2020-10-14 10:51:53 -0500

    Still not working at all

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/front-end/dist/`));
+app.use(express.static(`${__dirname}/dist/shop`));
 
 app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(`./dist/shop/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.listen(process.env.PORT || 8080);
